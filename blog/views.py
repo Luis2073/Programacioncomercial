3d9feadea32ed258211613edfa0a89731032fc75
Luis <martinez500chavez@gmail.com>
2019-09-02 12:06:38 -0600

    Se agrego una plantilla html Se leen los datos desde el modelo

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Publicaciones
+from django.utils import timezone
+
 # Create your views here.
+def listar_pub(request):
+    pubs = Publicaciones.objects.filter(fecha_publicacion__lte = timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/listar_pub.html', {'pubs': pubs})
